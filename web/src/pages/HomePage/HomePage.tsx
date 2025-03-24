e101b5c49fc0a773b0256096a741ae5d1caba4a3
Bill McCann <bill.mccann@gmail.com>
2025-03-24 07:29:51 -0400

    Update home page text

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import MainLayout from 'src/layouts/MainLayout/MainLayout'
 const HomePage = () => {
   return (
     <MainLayout>
-      <h2 className="text-2xl">Welcome to the Home Page</h2>
+      <h2 className="text-2xl">Hello World</h2>
       {/* Add more content here */}
     </MainLayout>
   )
